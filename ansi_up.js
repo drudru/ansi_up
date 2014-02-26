5c755d544adf78e8f1be0188700eacc1c13e18ba
James White <dev.jameswhite@gmail.com>
2014-02-26 18:21:44 +0000

    Bumping version number

--- ansi_up.js
+++ ansi_up.js
@@ -1,5 +1,5 @@
 // ansi_up.js
-// version : 1.0.0
+// version : 1.1.0
 // author : Dru Nelson
 // license : MIT
 // http://github.com/drudru/ansi_up
@@ -7,7 +7,7 @@
 (function (Date, undefined) {
 
     var ansi_up,
-        VERSION = "1.0.0",
+        VERSION = "1.1.0",
 
         // check for nodeJS
         hasModule = (typeof module !== 'undefined'),
